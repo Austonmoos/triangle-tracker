4b41624c667c627c36749de77883befa2b73196b
Auston Moos <AustonHMoos2004@gmail.com>
2022-08-11 15:59:59 -0700

    add code/syles/js Co-authored-by: Noah Cowan <nsncowan@gmail.com> Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  
+document.querySelector("form").onsubmit = function(e) {
+  e.preventDefault();
+const equilateral = parseInt(document.querySelector("input#sidea").value);
+const isosceles = parseInt(document.querySelector("input#sideb").value);
+const scalene = parseInt(document.querySelector("input#sidec").value);
+  }  
+if (a == b && b == c)
+equil
+
+else if (a == b || b == c || a == c)
+isosceles
+
+else if (a != b != c)
+scalene
+
+else 
+}
