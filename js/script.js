61b6dced882a0543b70fc31ee7fd2105f0c856ac
Auston Moos <AustonHMoos2004@gmail.com>
2022-08-11 17:20:15 -0700

    add code to javascript, moved html Co-authored-by: Alesandria Wild <alesandriawild@yahoo.com>

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-window.onload = function() {
-  
-document.querySelector("form").onsubmit = function(e) {
-  e.preventDefault();
-const equilateral = parseInt(document.querySelector("input#sidea").value);
-const isosceles = parseInt(document.querySelector("input#sideb").value);
-const scalene = parseInt(document.querySelector("input#sidec").value);
-  }  
-if (a == b && b == c)
-equil
+function checkTri(a, b, c) {
+const points = [a, b, c];
+points.sort(function(a, b){return b-a});
+a = points[0];
+b = points[1];
+c = points[2];
+  if (a > b + c) {
+  return "It will not Triangle! Refresh the page to try again!"
 
-else if (a == b || b == c || a == c)
-isosceles
-
-else if (a != b != c)
-scalene
-
-else 
+  }else if (a === b && b === c) {
+  return "It will Equailateral"
+ 
+  }else if (a === b || b === c || a === c) {
+  return "It will Isoceles"
+ 
+  }else if (a != b != c){
+  return "It will Scalene" 
+ };
 }
+
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("determine-triangle")
+  document.querySelector("form").onsubmit = function(e) {
+    e.preventDefault();
+  let a = parseInt(document.querySelector("input#inputa").value);
+  let b = parseInt(document.querySelector("input#inputb").value);
+  let c = parseInt(document.querySelector("input#inputc").value);
+  const result = checkTri(a, b, c);
+  document.getElementById("output").innerText = result;
+  
+  }
+});
